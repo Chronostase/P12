e843b085284e0b65431d356bee95176b04eecb6d
Thomas <thomas.giron@outlook.fr>
2021-01-06 17:27:45 +0100

    initial commit

---
+++ Unsinkable/Unsinkable/View/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Unsinkable
+//
+//  Created by Thomas on 06/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var age = 12
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
