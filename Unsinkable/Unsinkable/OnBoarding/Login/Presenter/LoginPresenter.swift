4319aa3ddf9a11579def03710b433ba94b3abaeb
Thomas <thomas.giron@outlook.fr>
2021-06-15 16:29:51 +0200

    change folder structure, update cocoapods and pods

---
+++ Unsinkable/Unsinkable/OnBoarding/Login/Presenter/LoginPresenter.swift
@@ -0,0 +1,50 @@
+//
+//  LoginPresenter.swift
+//  Unsinkable
+//
+//  Created by Thomas on 10/02/2021.
+//
+
+import Foundation
+
+protocol LoginPresenterDelegate: AnyObject {
+    func loginSucceed()
+    func loginFailed()
+    func emptyFields()
+}
+
+class LoginPresenter {
+
+    weak var delegate: LoginPresenterDelegate?
+    let userAuthenticationService: AuthentificationLogic = UserAuthentificationService()
+    
+    func checkTextFieldsAvailable(_ email: String?, _ password: String?) -> Bool {
+        if email != "" && password != "" {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func login(email: String?, password: String?) {
+        if checkTextFieldsAvailable(email, password) {
+            guard let email = email?.formatCharacter(), let password = password?.formatCharacter() else {
+                self.delegate?.loginFailed()
+                return
+            }
+            userAuthenticationService.loginUser(email, password) { [weak self] result in
+                switch result {
+                case .success(_):
+                    self?.delegate?.loginSucceed()
+                    return
+                    
+                case .failure(_):
+                    self?.delegate?.loginFailed()
+                    return
+                }
+            }
+        } else {
+            delegate?.emptyFields()
+        }
+    }
+}
